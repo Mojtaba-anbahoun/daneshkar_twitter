ca38b5859b1ec9f79bc5226a02062dc7a04cabb6
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-13 18:20:02 +0330

    add class user and class post

--- core/models.py
+++ core/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class Users(models.Model):
+    full_name = models.CharField(_("Nama:"), max_length=150)
+    user_name = models.CharField(_("User_Name:"), max_length=150)
+    password = models.IntegerField(_("Password:"), max_length=30)
+    bio = models.TextField(_("Biography:"))
+    image_profile = models.ImageField(_("Image:"), upload_to="images_file",
+                                      null=True, blank=True)
+    birth_date = models.DateField(_("Birth_date:"), null=True, blank=True)
+    email = models.EmailField(_("Email:"), 
+                              verbose_name="email address",
+                              max_length=255,
+                              unique=True,)
+    join_date = models.DateTimeField(_("Join_date:"))
+
+class Post(models.Model):
+    caption = models.CharField(_("Caption:"), max_length=100)
+    body = models.TextField(_("Body:"))
+    create_at = models.DateField(_("Create_at"))
+    image_url = models.ImageField(_("Image:"), upload_to="images_file",
+                                      null=True, blank=True)
+    
