451b1412963963eb1a1ee1088996099537625943
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-17 16:29:53 +0330

    create app accounts and contents

--- core/models.py
+++ core/models.py
@@ -11,10 +11,10 @@ class Users(models.Model):
     image_profile = models.ImageField(_("Image:"), upload_to="images_file",
                                       null=True, blank=True)
     birth_date = models.DateField(_("Birth_date:"), null=True, blank=True)
-    email = models.EmailField(_("Email:"), 
-                              verbose_name="email address",
-                              max_length=255,
-                              unique=True,)
+    #email = models.EmailField(_("Email:"), 
+    #                          verbose_name="email address",
+    #                          max_length=200,
+    #                          unique=True)
     join_date = models.DateTimeField(_("Join_date:"))
 
 class Post(models.Model):
@@ -24,3 +24,5 @@ class Post(models.Model):
     image_url = models.ImageField(_("Image:"), upload_to="images_file",
                                       null=True, blank=True)
     
+
+
