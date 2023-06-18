461d743679c6bc7f20dbd3dde98c1ffec81da9e1
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-18 21:59:57 +0330

    add contents/admin.py

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
-
+from .models import Post, Tag , Image, Comment, Category
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'user', 'category' , 'body']
+    search_fields = ['category', 'user']
+    list_filter = ['category', 'user']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'posts']
+    search_fields = ['text']
+    list_filter = ['posts']
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'alt', 'image', 'post']
+    search_fields = ['name']
+    list_filter = ['post']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'user', 'post', 'reply_to']
+    search_fields = ['user']
+    list_filter = ['post']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'slug']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+
+
+
--- contents/models.py
+++ contents/models.py
@@ -15,6 +15,9 @@ class Post(TimeStampMixin, BaseModel):
         on_delete=models.CASCADE,
         related_name = "posts",
     )
+    category = models.ForeignKey("Category" , on_delete=models.CASCADE, related_name="categories"
+
+    )    
     
     
 class Tag(models.Model):
@@ -23,6 +26,8 @@ class Tag(models.Model):
 
 
 class Image(models.Model):
+    name = models.CharField(verbose_name=_("Name"), max_length=30)
+    alt = models.CharField(verbose_name=_("Alternative text"), max_length=30)
     image = models.FileField(upload_to="uploads/images/")
     post = models.ForeignKey(Post , related_name="images", on_delete=models.CASCADE)
 
@@ -33,6 +38,13 @@ class Comment(TimeStampMixin):
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     reply_to = models.ForeignKey("self", blank=True, null=True , on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+
+    def __str__(self):
+        return f'comment on {self.user}'
 
 
 #class Reaction(BaseModel):
