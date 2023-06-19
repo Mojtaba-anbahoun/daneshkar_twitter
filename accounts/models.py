da4455a415a9fdf9d6a618599adff70dd9de10f5
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-19 18:17:29 +0330

    edit class user

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,6 @@ class User(models.Model):
         blank=False,
         null=False, 
     )
-
     user_name = models.CharField(
         verbose_name=_("User_Name:"),
         max_length=150,
@@ -22,29 +21,24 @@ class User(models.Model):
         db_index=True,
         help_text=_("Username to login and show in the profile"),
     )
-
     password = models.IntegerField(
         verbose_name=_("Password:"),
         help_text=_("Password to login"),
         blank=False,
         null=False,
     )
-
     image_profile = models.ImageField(
         verbose_name=_("Image:"),
         upload_to="uploads/photos/",
         null=True,
         blank=True,
     )
-
-
     bio = models.TextField(_("Biography:"))
     birth_date = models.DateField(_("Birth_date:"), null=True, blank=True,)
     email = models.EmailField(
        verbose_name=_("Email"),
        unique=True,
     )
-  
     join_date = models.DateTimeField(_("Join_date:"))
 
     class Meta:
