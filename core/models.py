d407d7e19bb3a36b418a9b27e626a54c2b3a06b0
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-19 18:18:48 +0330

    class meta TimeStampMixin

--- core/models.py
+++ core/models.py
@@ -6,10 +6,14 @@ from uuid import uuid4
 class BaseModel(models.Model):
     class meta:
         abstract = True
+
     id = models.UUIDField(editable=False, primary_key=True, default=uuid4)
 
 
 class TimeStampMixin:
+    class meta:
+        abstract = True
+        
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
