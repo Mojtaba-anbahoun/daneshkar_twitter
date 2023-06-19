bbd4f680666f09974b2d14897dbc5c6c7f45c636
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-19 18:15:42 +0330

    add user to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User , Relation
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'full_name', 'user_name', 'email' , 'bio', 'birth_date', 'join_date']
+    search_fields = ['user_name', 'email']
+    list_filter = ['user_name', 'email']
+
+#@admin.register(Relation)
+#class RelationAdmin(admin.ModelAdmin):
+#    list_display = ['id', 'from_user', 'to_user']
+#    search_fields = ['from_user', 'to_user']
+#    list_filter = ['from_user', 'to_user']
