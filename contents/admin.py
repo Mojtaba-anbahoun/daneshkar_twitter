d78a1c7f977510f541176d2c08444049f35752c3
Mojtaba-anbahoun <mojtaba.anbahoun@gmail.com>
2023-06-19 18:33:08 +0330

    create base.html

--- contents/admin.py
+++ contents/admin.py
@@ -24,11 +24,11 @@ class ImageAdmin(admin.ModelAdmin):
     list_filter = ['post']
 
 
-#@admin.register(Comment)
-#class CommentAdmin(admin.ModelAdmin):
-#    list_display = ['id', 'text', 'user', 'post', 'reply_to']
-#    search_fields = ['user']
-#    list_filter = ['post']
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'user', 'post', 'reply_to']
+    search_fields = ['user']
+    list_filter = ['post']
 
 
 @admin.register(Category)
@@ -38,6 +38,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['name']
 
 
+#@admin.register(Reaction)
+#class ReactionAdmin(admin.ModelAdmin):
+#    list_display = ['id', 'user', 'post']
+#    search_fields = ['user', 'post']
+#    list_filter = ['user', 'post']
 
 
 
